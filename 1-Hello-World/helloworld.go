71be2d5cf3891ef948741f5f2b28749ddad834eb
Lucas Primo <lucas.primo@hpe.com>
2022-02-17 09:37:22 -0300

    Hello World with Go

---
+++ 1-Hello-World/helloworld.go
@@ -0,0 +1,11 @@
+package main // package main identifies the Go executable
+
+// libraries that we will use in the program
+import (
+	"fmt"
+)
+
+// main function also identifies the Go executable
+func main() {
+	fmt.Println("Hello World!")
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning-Go
 Repository to Learn Go from Udemy course
+
+# How to run a Go file
+go run <filename>
