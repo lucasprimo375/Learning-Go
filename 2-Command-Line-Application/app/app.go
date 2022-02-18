f8f694374a215432d385f68be9c36f22063ca418
Lucas Primo <lucas.primo@hpe.com>
2022-02-18 20:48:23 -0300

    Implementing host server search

--- 2-Command-Line-Application/app/app.go
+++ 2-Command-Line-Application/app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Searches for IPs and servers names on the web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IP address on the web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IP address on the web",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search server name on the web",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 
@@ -37,10 +45,24 @@ func searchIps(c *cli.Context) {
 	ips, error := net.LookupIP(host)
 
 	if error != nil {
-		log.Fatal("error")
+		log.Fatal(error)
 	}
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServer(c *cli.Context) {
+	host := c.String("host")
+
+	servers, error := net.LookupNS(host)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
