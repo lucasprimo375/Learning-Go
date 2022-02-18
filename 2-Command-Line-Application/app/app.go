1f922b87a3ed94db7080c46ecdd5074711aee77e
Lucas Primo <lucas.primo@hpe.com>
2022-02-18 20:41:45 -0300

    Implementing ip search

--- 2-Command-Line-Application/app/app.go
+++ 2-Command-Line-Application/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate returns the command line application ready to be executed
 func Generate() *cli.App {
@@ -8,5 +14,33 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Searches for IPs and servers names on the web"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IP address on the web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+
+	if error != nil {
+		log.Fatal("error")
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
