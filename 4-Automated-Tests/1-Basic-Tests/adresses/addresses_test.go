f6325f2b22f7ba8d88c5abb1682288b0e0f3574b
Lucas Primo <lucas.primo@hpe.com>
2022-02-19 16:18:33 -0300

    More test cases

--- 4-Automated-Tests/1-Basic-Tests/adresses/addresses_test.go
+++ 4-Automated-Tests/1-Basic-Tests/adresses/addresses_test.go
@@ -4,14 +4,30 @@ package adresses
 
 import "testing"
 
+type testScenario struct {
+	receivedAddress string
+	expectedAddress string
+}
+
 func TestTypeOfAddress(t *testing.T) {
-	testAddress := "Street Paulista"
+	testScenarios := []testScenario{
+		{"Street ABC", "Street"},
+		{"Avenue Paulista", "Avenue"},
+		{"Road of the Fools", "Road"},
+		{"Highway of Highwater", "Highway"},
+		{"Pool Form", "Invalid Address"},
+		{"", "Invalid Address"},
+		{"HIGHWAY CHAGAS", "Highway"},
+		{"Road 31", "Road"},
+		{"Avenue Town", "Avenue"},
+		{"Street Leeds", "Street"},
+	}
 
-	expectedAddressType := "Street"
+	for _, scenario := range testScenarios {
+		receivedAddressType := TypeOfAddress(scenario.receivedAddress)
 
-	receivedAddressType := TypeOfAddress(testAddress)
-
-	if receivedAddressType != expectedAddressType {
-		t.Errorf("Received address type is different than the expected. Expected %s, received %s", expectedAddressType, receivedAddressType)
+		if receivedAddressType != scenario.expectedAddress {
+			t.Errorf("Received type %s is different than expected type %s", receivedAddressType, scenario.expectedAddress)
+		}
 	}
 }
