01b5c883e11e53d3b97e11c6e958c8391f629779
Lucas Primo <lucas.primo@hpe.com>
2022-02-19 12:20:50 -0300

    Generator pattern
    To be used when we wnat to encapsulate a goroutine

---
+++ 3-Concurrency/7-Generator-Pattern/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	channel := write("Hello world!")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-channel)
+	}
+}
+
+func write(text string) <-chan string {
+	channel := make(chan string)
+
+	go func() {
+		for {
+			channel <- fmt.Sprintf("Got: %s", text)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return channel
+}
