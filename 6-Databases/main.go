6355a527be092c042905fa4c929e613c8bb1777a
Lucas Primo <lucas.primo@hpe.com>
2022-02-20 15:13:52 -0300

    Opening connection with database and making simple query

---
+++ 6-Databases/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	connectionString := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, error := sql.Open("mysql", connectionString)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer db.Close()
+
+	error = db.Ping()
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Println("Connection open")
+
+	rows, error := db.Query("select * from users")
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer rows.Close()
+
+	fmt.Println(rows)
+}
