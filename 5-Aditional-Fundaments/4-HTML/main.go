20b05edc92e488f4da34f808ab52be494f507841
Lucas Primo <lucas.primo@hpe.com>
2022-02-20 12:34:11 -0300

    Serving simple HTML page with data via HTTP

--- 5-Aditional-Fundaments/4-HTML/main.go
+++ 5-Aditional-Fundaments/4-HTML/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	u := user{"Lucas", "lucas@email.com"}
+
+	templates.ExecuteTemplate(w, "home.html", u)
+}
+
+type user struct {
+	Name  string
+	Email string
+}
+
 func main() {
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", home)
+
+	fmt.Println("Listening on port 5000")
+
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
