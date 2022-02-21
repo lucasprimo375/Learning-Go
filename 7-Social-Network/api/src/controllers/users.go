02647c18dcd6377e700493512c178234967cf879
Lucas Primo <lucas.primo@hpe.com>
2022-02-21 14:55:50 -0300

    Creating repository package

--- 7-Social-Network/api/src/controllers/users.go
+++ 7-Social-Network/api/src/controllers/users.go
@@ -1,23 +1,49 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repository"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func CreateUser(w http.ResponseWriter, e *http.Request) {
-	w.Write([]byte("Creating user"))
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	requestBody, error := ioutil.ReadAll(r.Body)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	var user models.User
+
+	error = json.Unmarshal(requestBody, &user)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	db, error := database.Connect()
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	repository := repository.NewUsersRepository(db)
+	repository.Create(user)
 }
 
-func GetUsers(w http.ResponseWriter, e *http.Request) {
+func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Getting all users"))
 }
 
-func GetUser(w http.ResponseWriter, e *http.Request) {
+func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Getting user"))
 }
 
-func UpdateUser(w http.ResponseWriter, e *http.Request) {
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Updating user"))
 }
 
-func DeleteUser(w http.ResponseWriter, e *http.Request) {
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deleting user"))
 }
