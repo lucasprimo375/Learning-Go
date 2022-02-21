3bbd686364bac88df129637014a7e29766786d0a
Lucas Primo <lucas.primo@hpe.com>
2022-02-21 08:52:33 -0300

    Gettign data from the database
    Getting all rows from a table

--- 6-Databases/2-Basic-CRUD/server/server.go
+++ 6-Databases/2-Basic-CRUD/server/server.go
@@ -59,3 +59,45 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("User inserted ID %d successfully", insertedId)))
 }
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	db, error := database.Connect()
+	if error != nil {
+		w.Write([]byte("Failed to connect to database"))
+		return
+	}
+	defer db.Close()
+
+	rows, error := db.Query("select * from users")
+	if error != nil {
+		w.Write([]byte("Failed to get users"))
+		return
+	}
+	defer rows.Close()
+
+	var users []user
+
+	for rows.Next() {
+		var user user
+
+		error := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if error != nil {
+			w.Write([]byte("Failed to scan user"))
+			return
+		}
+
+		users = append(users, user)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	error = json.NewEncoder(w).Encode(users)
+	if error != nil {
+		w.Write([]byte("Failed to convert users to json"))
+		return
+	}
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
