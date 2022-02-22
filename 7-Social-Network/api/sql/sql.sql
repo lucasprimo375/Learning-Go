6b39cb63fe9fae0474f6b4cbbc420f8a63e9e181
Lucas Primo <lucas.primo@hpe.com>
2022-02-22 15:10:45 -0300

    Creating followers table

--- 7-Social-Network/api/sql/sql.sql
+++ 7-Social-Network/api/sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
+DROP TABLE IF EXISTS followers;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -11,4 +12,14 @@ CREATE TABLE users (
     email varchar(50) not null unique,
     password varchar(100) not null,
     creationDate timestamp default current_timestamp()
+) ENGINE = INNODB;
+
+CREATE TABLE followers(
+    user_id int not null,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    follower_id int not null,
+    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    primary key(user_id, follower_id)
 ) ENGINE = INNODB;
