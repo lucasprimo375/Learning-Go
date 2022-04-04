53102b04f058d4ee8fd63c719f8db43af66af290
Lucas Primo <lucas.primo@hpe.com>
2022-04-04 14:21:11 -0300

    Creating publications model and table

--- 7-Social-Network/api/sql/sql.sql
+++ 7-Social-Network/api/sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
+DROP TABLE IF EXISTS publications;
 DROP TABLE IF EXISTS followers;
 DROP TABLE IF EXISTS users;
 
@@ -22,4 +23,16 @@ CREATE TABLE followers(
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
 
     primary key(user_id, follower_id)
+) ENGINE = INNODB;
+
+CREATE TABLE publications(
+    id int auto_increment primary key,
+    title varchar(50) not null,
+    content varchar(300) not null,
+    
+    author_id int not null,
+    foreign key (author_id) references users(id) ON DELETE CASCADE,
+
+    likes int default 0,
+    creationDate timestamp default current_timestamp()
 ) ENGINE = INNODB;
