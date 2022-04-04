25fe0e8dca0c30b4f43ff04700893f522ae6cd87
Lucas Primo <lucasprimo375@hotmail.com>
2022-04-04 15:08:50 -0300

    Implementing publication creation

--- 7-Social-Network/api/src/controllers/publication.go
+++ 7-Social-Network/api/src/controllers/publication.go
@@ -1,8 +1,55 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/authentication"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repository"
+	"api/src/responses"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
-func CreatePublication(w http.ResponseWriter, r *http.Request) {}
+func CreatePublication(w http.ResponseWriter, r *http.Request) {
+	userID, err := authentication.ExtractUserID(r)
+	if err != nil {
+		responses.Error(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var publication models.Publication
+	err = json.Unmarshal(requestBody, &publication)
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	publication.AuthorID = userID
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repository.NewPublicationsRepository(db)
+
+	publication.ID, err = repository.Create(publication)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, publication)
+}
 
 func GetPublications(w http.ResponseWriter, r *http.Request) {}
 
