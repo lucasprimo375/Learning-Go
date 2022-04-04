eed793c05679943e2f1fe9eaed04462343cbe004
Lucas Primo <lucasprimo375@hotmail.com>
2022-04-04 14:40:05 -0300

    Creating publication routes

---
+++ 7-Social-Network/api/src/controllers/publication.go
@@ -0,0 +1,13 @@
+package controllers
+
+import "net/http"
+
+func CreatePublication(w http.ResponseWriter, r *http.Request) {}
+
+func GetPublications(w http.ResponseWriter, r *http.Request) {}
+
+func GetPublication(w http.ResponseWriter, r *http.Request) {}
+
+func UpdatePublication(w http.ResponseWriter, r *http.Request) {}
+
+func DeletePublication(w http.ResponseWriter, r *http.Request) {}
---
+++ 7-Social-Network/api/src/router/routes/publications.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var publicationsRoutes = []Route{
+	{
+		URI:                    "/publications",
+		Method:                 http.MethodPost,
+		Function:               controllers.CreatePublication,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/publications",
+		Method:                 http.MethodGet,
+		Function:               controllers.GetPublications,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/publications/{publicationID}",
+		Method:                 http.MethodGet,
+		Function:               controllers.GetPublication,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/publications/{publicationID}",
+		Method:                 http.MethodPut,
+		Function:               controllers.UpdatePublication,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/publications/{publicationID}",
+		Method:                 http.MethodDelete,
+		Function:               controllers.DeletePublication,
+		RequiresAuthentication: true,
+	},
+}
