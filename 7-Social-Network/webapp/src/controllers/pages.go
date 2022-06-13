2ab69dd1d0497e0abdc9ddad2cc93e375f75fb7d
Lucas Muraro <lucasprimo375@hotmail.com>
2022-06-13 16:17:27 -0300

    Making requests with authentication

--- 7-Social-Network/webapp/src/controllers/pages.go
+++ 7-Social-Network/webapp/src/controllers/pages.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/requests"
 	"webapp/src/utils"
 )
 
@@ -15,5 +18,11 @@ func LoadCreateUserPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoadMainPage(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/publications", config.APIURL)
+
+	response, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+
+	fmt.Println(response, err)
+
 	utils.ExecuteTemplate(w, "home.html", nil)
 }
