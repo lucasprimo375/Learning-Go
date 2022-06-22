b8c29a68c52258499b8178c2c42068520421986f
Lucas Muraro <lucasprimo375@hotmail.com>
2022-06-22 16:34:35 -0300

    Adjusting navbar and creating templates for navbar, footer and scripts

--- 7-Social-Network/webapp/src/utils/templates.go
+++ 7-Social-Network/webapp/src/utils/templates.go
@@ -9,7 +9,8 @@ var templates *template.Template
 
 // LoadTemplates inserts HTML templates into the variable templates
 func LoadTemplates() {
-	templates = template.Must(templates.ParseGlob("views/*.html"))
+	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // ExecuteTemplate renders a HTML page on the browser
