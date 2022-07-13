4afb4ffbd449f33b69f6cda05553e70e4df56ba8
Lucas Muraro <lucasprimo375@hotmail.com>
2022-07-13 09:16:21 -0300

    Implemeting functions to get all user data

--- 7-Social-Network/webapp/src/models/user.go
+++ 7-Social-Network/webapp/src/models/user.go
@@ -1,8 +1,12 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
+	"webapp/src/config"
+	"webapp/src/requests"
 )
 
 type User struct {
@@ -26,20 +30,90 @@ func GetCompleteUser(userID uint64, r *http.Request) (User, error) {
 	go GetFollowers(followersChannel, userID, r)
 	go GetFollowing(followingChannel, userID, r)
 	go GetPublications(publicationsChannel, userID, r)
+
+	return User{}, nil
 }
 
-func GetUserData(channel <-chan User, userID uint64, r *http.Request) {
+func GetUserData(channel chan<- User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d", config.APIURL, userID)
 
+	response, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		channel <- User{}
+		return
+	}
+	defer response.Body.Close()
+
+	var user User
+
+	err = json.NewDecoder(response.Body).Decode(&user)
+	if err != nil {
+		channel <- User{}
+		return
+	}
+
+	channel <- user
 }
 
-func GetFollowers(channel <-chan []User, userID uint64, r *http.Request) {
+func GetFollowers(channel chan<- []User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/followers", config.APIURL, userID)
 
+	response, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var followers []User
+
+	err = json.NewDecoder(response.Body).Decode(&followers)
+	if err != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- followers
 }
 
-func GetFollowing(channel <-chan []User, userID uint64, r *http.Request) {
+func GetFollowing(channel chan<- []User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/following", config.APIURL, userID)
 
+	response, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var following []User
+
+	err = json.NewDecoder(response.Body).Decode(&following)
+	if err != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- following
 }
 
-func GetPublications(channel <-chan []Publication, userID uint64, r *http.Request) {
+func GetPublications(channel chan<- []Publication, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/publications", config.APIURL, userID)
 
+	response, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var publications []Publication
+
+	err = json.NewDecoder(response.Body).Decode(&publications)
+	if err != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- publications
 }
